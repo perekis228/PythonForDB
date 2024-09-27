96373a1d0555cf0dc96b518889d1b61c1ee38ad1
Alex_Black <37541820+perekis228@users.noreply.github.com>
2024-09-27 16:06:53 +0200

    Update task2.py

--- lab3/task2.py
+++ lab3/task2.py
@@ -8,9 +8,18 @@ red = hist[0:256]
 green = hist[256:512]
 blue = hist[512:768]
 
+fg = plt.figure(figsize=(9, 4), constrained_layout=True)
+gs = fg.add_gridspec(4, 2)
+
+fg.add_subplot(gs[:, 0])
 plt.imshow(img)
-fig, axs = plt.subplots(nrows=4)
+plt.xticks([])
+plt.yticks([])
+
 for num, color_name, color in (0, 'black', hist), (1, 'red', red), (2, 'green', green), (3, 'blue', blue):
-    axs[num].plot(color, c=color_name)
-    axs[num].set_title(color_name)
+    fg.add_subplot(gs[num, 1])
+    plt.plot(color, c=color_name)
+    plt.xticks([])
+    plt.yticks([])
+    plt.axis('off')
 plt.show()
