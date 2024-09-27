22b325066a517ac4519a9ebb4fc0e3f590b2f091
Alex_Black <37541820+perekis228@users.noreply.github.com>
2024-09-27 15:42:06 +0200

    Update task1.py

--- lab3/task1.py
+++ lab3/task1.py
@@ -2,21 +2,22 @@
 import os
 import argparse
 from skimage import io, transform, exposure
+import numpy as np
 
 def clockwise(img):
-    return transform.rotate(img, angle=-45, resize=True)
+    return (transform.rotate(img, angle=-45, resize=True)*255).astype(np.uint8)
 
 def counterclockwise(img):
-    return transform.rotate(img, angle=45, resize=True)
+    return (transform.rotate(img, angle=45, resize=True)*255).astype(np.uint8)
 
 def flip(img):
     return img[::-1]
 
 def swirl(img):
-    return transform.swirl(img, strength=10)
+    return (transform.swirl(img, strength=10)*255).astype(np.uint8)
 
 def gamma(img):
-    return exposure.adjust_gamma(img, gamma=0.5)
+    return (exposure.adjust_gamma(img, gamma=0.5)*255).astype(np.uint8)
 
 def complex(img):
     return gamma(swirl(flip(img)))
@@ -29,12 +30,24 @@ def main(input_dir, operation):
                   'gamma': gamma,
                   'complex': complex}
 
-    cur_oper = operations[operation]
+    try:
+        cur_oper = operations[operation]
+    except KeyError:
+        raise Exception(f'There is no such operation "{operation}"')
+
+    next_num = int(os.listdir(input_dir)[-1][:4])+1
+    str_num = ''
+
     for filename in os.listdir(input_dir):
-        print(input_dir + '/' + filename)
         img = io.imread((input_dir + '/' + filename))
         res = cur_oper(img)
-        io.imsave(input_dir + f'/new_{filename}', res.astype('uint8'))
+        for i in range(1, 5):
+            if next_num < 10**i:
+                str_num = '0'*(4-i) + str(next_num)
+                break
+        print(str_num+'.jpg')
+        io.imsave(input_dir + f'/{str_num}.jpg', res)
+        next_num += 1
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Thumbnail Viewer")
